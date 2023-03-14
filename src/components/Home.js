c76733a8729868c475ccf0580ead5912e98c6018
Umair Shah <ushah6442@gmail.com>
2023-03-14 07:30:37 +0500

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,140 @@
+import React from 'react'
+import './homestyle.css'
+import styled from 'styled-components'
+function Home() {
+    return (
+      <div className='main'>
+        <div className='firstrow'>
+      <p>Developed by Umair</p>
+      <button>Resume</button>
+        </div>
+      <div className='sectiontwo'>
+<h1>Umair Shoukat</h1>
+<h2>Software developer</h2>
+<p>Freelancer providing services for Mobile app development and webdevelopment join down below </p>
+<div className='icons'>
+<a href="https://www.facebook.com/saim.umair.5/" title="Visit Facebook"><div  className='one' > </div> </a>
+<a href="https://www.linkedin.com/in/umair-shah-5b6b51228/" title="Visit LinkedIn"><div  className='two' > </div> </a>
+<a href="https://github.com/Umair2859"><div  className='three' title="Visit Github" > </div> </a>
+</div>
+</div>
+<div className='section3'>
+<div className='avatar'></div>
+
+      </div>
+       <div className='section4'>
+<h1>Services I offer</h1>
+<p>Since the begining of my journey as a freelancer and a developer, I have completed several projects for my clients in different technologies</p>
+<p>I offer from a wide range of services including design , programming and teaching</p>
+</div>
+<div className='section5'>
+<div className='card1'>
+<div className='flutter'>
+    
+</div>
+<strong style={{color:'black'}}>Native android development</strong>
+<p style={{color:'black'}}>Creating native android apps for your desired requirements</p>
+<strong style={{color:'black'}}>Tools I use</strong>
+<p style={{color:'black'}}>Android studio</p>
+<p style={{color:'black'}}>Xml</p>
+<p style={{color:'black'}}>Java</p>
+<p style={{color:'black'}}>Firebse</p>
+
+</div>
+
+<div className='card1'>
+<div className='android'>
+    
+</div>
+<strong style={{color:'black'}}>Flutter app development development</strong>
+<p style={{color:'black'}}>Creating beautiful flutter apps for multiple platforms</p>
+<strong style={{color:'black'}}>Tools I use</strong>
+<p style={{color:'black'}}>Vs code</p>
+<p style={{color:'black'}}>Flutter</p>
+<p style={{color:'black'}}>Dart</p>
+<p style={{color:'black'}}>Firebse</p>
+
+</div>
+<div className='card1'>
+<div className='web'>
+    
+</div>
+<strong style={{color:'black'}}>Webdevelopment</strong>
+<p style={{color:'black', marginLeft:'30px'}}>Eyecatching and user friendly websites according to your requirements </p>
+<strong style={{color:'black'}}>Tools I use</strong>
+<p style={{color:'black'}}>Reactjs</p>
+<p style={{color:'black'}}>Html</p>
+<p style={{color:'black'}}>Css</p>
+<p style={{color:'black'}}>Javascript</p>
+
+</div>
+
+</div>
+
+<div className='section7'>
+<h1>Projects</h1>
+<p>Here are some screenshots of the products I have developed with differnt technologies</p>
+<p >I offer from a wide range of services including design , programming and teaching</p>
+<div className='project1'>
+<div className='picture1'></div>
+<div className='picture2'></div>
+      </div>
+      <div className='project2'>
+<div className='picture3'></div>
+<div className='picture4'></div>
+      </div>
+      <div className='project3'>
+<div className='picture5'></div>
+<div className='picture6'></div>
+      </div>
+
+      
+      
+      </div> 
+      <div className='foot'>
+   
+      <div className='firstd'>
+      <h1>About</h1>
+            <p>Umair Shoukat</p>
+            <p>us392478@gmail.com</p>
+      </div>
+      <div className='firstd'>
+      <h1>Services</h1>
+            <p>Programming</p>
+            <p>Mobile App development</p>
+      </div>
+      <div className='firstd'>
+      <h1>Socialmedia</h1>
+            <p>Facebook</p>
+            <p>Github</p>
+            <p>Portfolio</p>
+            
+            
+
+            
+      </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </div>
+      
+    )
+  }
+  export default Home
+  
